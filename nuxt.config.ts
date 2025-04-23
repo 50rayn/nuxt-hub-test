73fd880f45bdb5c441af523641e0f4e60be8cb5c
Soryn <soryngitlan@gmail.com>
2025-04-23 22:33:46 +0300

    chore: set workers flag to true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
     database: true,
-    workers: false
+    workers: true
   },
 
   // Development config
